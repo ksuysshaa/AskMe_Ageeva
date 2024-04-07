63cbc5b3c59646a0b542d9c9839852260eb9825f
ksuysshaa <k.ageeva0505@gmail.com>
2024-04-07 21:28:08 +0300

    feat: added django templates

--- app/views.py
+++ app/views.py
@@ -1,24 +1,52 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 
-# Create your views here.
+TAGS = [
+    {
+        "id": i,
+        "title": f"Tag {i}",
+        "text": f"This is tag number {i}"
+    } for i in range(5)
+]
+
+MEMBERS = [
+    {
+        "id": i,
+        "title": f"Member {i}",
+    } for i in range(5)
+]
 
 QUESTIONS = [
     {
         "id": i,
         "title": f"Question {i}",
-        "text": f"This is question number {i}"
-    } for i in range(200)
+        "text": f"This is question number {i}",
+        "tag": TAGS[i % 3].get('name'),
+        "tagId": i % 3
+    } for i in range(100)
 ]
 
-def index(request):
+PER_PAGE = 10
+
+
+def paginate(objects, request, per_page=PER_PAGE):
     page_num = request.GET.get('page', 1)
-    paginator = Paginator(QUESTIONS, 5)
+    paginator = Paginator(objects, per_page)
     page_obj = paginator.page(page_num)
-    return render(request, 'index.html', {"questions": page_obj})
+    return page_obj
+
+# Create your views here.
+
+
+def index(request):
+    page_obj = paginate(QUESTIONS, request, PER_PAGE)
+    return render(request, 'index.html', {"questions": page_obj, "members": MEMBERS, "tags": TAGS})
+
 
 def hot(request):
-    return render(request, 'hot.html', {"questions": QUESTIONS})
+    page_obj = paginate(QUESTIONS, request, PER_PAGE)
+    return render(request, 'hot.html', {"questions": page_obj, "members": MEMBERS, "tags":  TAGS})
+
 
 def question(request, question_id):
     question = QUESTIONS[question_id]
@@ -26,7 +54,38 @@ def question(request, question_id):
         {
             'id': i,
             'title': f'Answer {i}',
-            'content': f'Long lorem ipsum {i}'
-        } for i in range(5)
+            'content': f'Answer text {i}'
+        } for i in range(30)
     ]
-    return render(request, 'question.html', {"question": question, "answers": answers})
+    answers_page_obj = paginate(answers, request, PER_PAGE)
+    return render(request, 'question.html', {"question": question, "answers": answers_page_obj, "members": MEMBERS, "tags": TAGS})
+
+
+def ask(request):
+    return render(request, 'ask.html', {"questions": QUESTIONS, "members": MEMBERS, "tags": TAGS})
+
+
+def login(request):
+    return render(request, 'login.html', {"members": MEMBERS, "tags": TAGS})
+
+
+def signup(request):
+    return render(request, 'signup.html', {"members": MEMBERS, "tags": TAGS})
+
+
+def tag(request):
+    tag = request.GET.get('tag', 0)
+    questions = []
+    for i in range(0, len(QUESTIONS)):
+        if QUESTIONS[i].get('tag') == TAGS[int(tag)].get('name'):
+            questions.append(QUESTIONS[i])
+    page_obj = paginate(questions, request, PER_PAGE)
+    return render(request, 'tag.html', {"questions": page_obj, 'tag': TAGS[int(tag)], "members": MEMBERS, "tags": TAGS, 'route':  "tags/?tag=" + str(tag), 'symbolRoute': "&"})
+
+
+def logout(request):
+    return render(request, 'login.html', {"members": MEMBERS, "tags": TAGS})
+
+
+def settings(request):
+    return render(request, 'settings.html', {"members": MEMBERS, "tags": TAGS})
--- askme_ageeva/urls.py
+++ askme_ageeva/urls.py
@@ -23,4 +23,10 @@ urlpatterns = [
     path('admin/', admin.site.urls, name='admin-panel'),
     path('hot/', views.hot, name='hot'),
     path('question/<int:question_id>', views.question, name='question'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('ask/', views.ask, name='ask'),
+    path('tags/', views.tag, name='tags'),
+    path('logout/', views.logout, name='logout'),
+    path('settings/', views.settings, name='settings')
 ]
